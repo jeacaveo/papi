e6b6a0b4827d47cf88cea209e3de7af52ea6b0e1
Jean Ventura <jv@venturasystems.net>
2019-11-19 07:39:17 -0800

    Add initial drf_yasg configuration

--- papi/urls.py
+++ papi/urls.py
@@ -17,10 +17,42 @@ from django.contrib import admin
 from django.urls import (
     include,
     path,
+    re_path,
+    )
+
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+from papi.views import RootView
+
+
+SCHEMA_VIEW = get_schema_view(
+    openapi.Info(
+        title="Prismata API",
+        default_version='v1',
+        description="REST API for Prismata related data.",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="jv@venturasystems.net"),
+        license=openapi.License(name="AGPLv3+ License"),
+        ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
     )
 
 
 urlpatterns = [  # pylint: disable=invalid-name
-    path("admin/", admin.site.urls),
-    path("api/auth/", include("rest_framework.urls"))
+    path("api/auth/", include("rest_framework.urls")),
+    re_path(
+        r'^swagger(?P<format>\.json|\.yaml)$',
+        SCHEMA_VIEW.without_ui(cache_timeout=0),
+        name='schema-json'),
+    re_path(
+        r'^swagger/$',
+        SCHEMA_VIEW.with_ui('swagger', cache_timeout=0),
+        name='schema-swagger-ui'),
+    re_path(
+        r'^redoc/$',
+        SCHEMA_VIEW.with_ui('redoc', cache_timeout=0),
+        name='schema-redoc'),
     ]
