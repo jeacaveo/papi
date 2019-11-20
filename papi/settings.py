3ea5733c55406719fb56fd4e7d0c4858d1fb7189
Jean Ventura <jv@venturasystems.net>
2019-11-19 16:24:10 -0800

    Add rest_framework settings

--- papi/settings.py
+++ papi/settings.py
@@ -128,3 +128,99 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = "/static/"
+
+REST_FRAMEWORK = {
+    # Base API policies
+    "DEFAULT_RENDERER_CLASSES": [
+        "rest_framework.renderers.JSONRenderer",
+        "rest_framework.renderers.BrowsableAPIRenderer",
+        ],
+    "DEFAULT_PARSER_CLASSES": [
+        "rest_framework.parsers.JSONParser",
+        "rest_framework.parsers.FormParser",
+        "rest_framework.parsers.MultiPartParser"
+        ],
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework.authentication.BasicAuthentication"
+        ],
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.AllowAny",
+        ],
+    "DEFAULT_THROTTLE_CLASSES": [
+        "rest_framework.throttling.AnonRateThrottle",
+        "rest_framework.throttling.UserRateThrottle",
+        ],
+    "DEFAULT_CONTENT_NEGOTIATION_CLASS":
+    "rest_framework.negotiation.DefaultContentNegotiation",
+    "DEFAULT_METADATA_CLASS": "rest_framework.metadata.SimpleMetadata",
+    "DEFAULT_VERSIONING_CLASS": None,
+
+    # Generic view behavior
+    "DEFAULT_PAGINATION_CLASS": None,
+    "DEFAULT_FILTER_BACKENDS": [],
+
+    # Schema
+    "DEFAULT_SCHEMA_CLASS": "rest_framework.schemas.openapi.AutoSchema",
+
+    # Throttling
+    "DEFAULT_THROTTLE_RATES": {
+        "anon": "600/minute",
+        "user": "6000/minute",
+        },
+    "NUM_PROXIES": None,
+
+    # Pagination
+    "PAGE_SIZE": None,
+
+    # Filtering
+    "SEARCH_PARAM": "search",
+    "ORDERING_PARAM": "ordering",
+
+    # Versioning
+    "DEFAULT_VERSION": None,
+    "ALLOWED_VERSIONS": None,
+    "VERSION_PARAM": "version",
+
+    # Authentication
+    "UNAUTHENTICATED_USER": "django.contrib.auth.models.AnonymousUser",
+    "UNAUTHENTICATED_TOKEN": None,
+
+    # View configuration
+    "VIEW_NAME_FUNCTION": "rest_framework.views.get_view_name",
+    "VIEW_DESCRIPTION_FUNCTION": "rest_framework.views.get_view_description",
+
+    # Exception handling
+    "EXCEPTION_HANDLER": "rest_framework.views.exception_handler",
+    "NON_FIELD_ERRORS_KEY": "non_field_errors",
+
+    # Testing
+    "TEST_REQUEST_RENDERER_CLASSES": [
+        "rest_framework.renderers.MultiPartRenderer",
+        "rest_framework.renderers.JSONRenderer"
+        ],
+    "TEST_REQUEST_DEFAULT_FORMAT": "multipart",
+
+    # Hyperlink settings
+    "URL_FORMAT_OVERRIDE": "format",
+    "FORMAT_SUFFIX_KWARG": "format",
+    "URL_FIELD_NAME": "url",
+
+    # Encoding
+    "UNICODE_JSON": True,
+    "COMPACT_JSON": True,
+    "STRICT_JSON": True,
+    "COERCE_DECIMAL_TO_STRING": True,
+    "UPLOADED_FILES_USE_URL": True,
+
+    # Browseable API
+    "HTML_SELECT_CUTOFF": 1000,
+    "HTML_SELECT_CUTOFF_TEXT": "More than {count} items...",
+
+    # Schemas
+    "SCHEMA_COERCE_PATH_PK": True,
+    "SCHEMA_COERCE_METHOD_NAMES": {
+        "retrieve": "read",
+        "destroy": "delete"
+        },
+    }
