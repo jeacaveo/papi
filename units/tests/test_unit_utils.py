4cc24a87a512f6e1c731a1ea832028e7c938c5a0
Jean Ventura <jv@venturasystems.net>
2019-11-22 07:20:01 -0800

    Add units.utils.filter_to_lookup function

--- units/tests/test_unit_utils.py
+++ units/tests/test_unit_utils.py
@@ -2,6 +2,7 @@
 import unittest
 
 from units.utils import (
+    filter_to_lookup,
     parse_query,
     )
 
@@ -9,33 +10,33 @@ from units.utils import (
 class ParseQueryDirtyTests(unittest.TestCase):
     """ Tests fail cases for units.utils.parse_query """
 
-    def test_queries(self):
-        """ Test all possible failures. """
+    def test_invalids(self):
+        """ Test all cases that include failures. """
         # Given
         data = {
             "mixed_end_invalid": {
                 "query": "gold=5, 5",
-                "expected_url": [['gold', '=', '5']]
+                "expected_result": [["gold", "=", "5"]]
                 },
             "mixed_start_invalid": {
                 "query": "5, gold=5",
-                "expected_url": []
+                "expected_result": []
                 },
             "mixed_middle_invalid": {
                 "query": "gold=5, 1, red!=2",
-                "expected_url": [['gold', '=', '5']]
+                "expected_result": [["gold", "=", "5"]]
                 },
             "single_empty": {
                 "query": "gold=5,",
-                "expected_url": [['gold', '=', '5']]
+                "expected_result": [["gold", "=", "5"]]
                 },
             "no_value": {
                 "query": "gold=",
-                "expected_url": [['gold']]
+                "expected_result": [["gold"]]
                 },
             "empty": {
                 "query": "",
-                "expected_url": []
+                "expected_result": []
                 },
             }
 
@@ -44,32 +45,32 @@ class ParseQueryDirtyTests(unittest.TestCase):
             with self.subTest(name):
                 self.assertEqual(
                     list(parse_query(params["query"])),
-                    params["expected_url"])
+                    params["expected_result"])
 
 
 class ParseQueryCleanTests(unittest.TestCase):
-    """ Tests success cases for units.models.LatestUnitVersionView model. """
+    """ Tests success cases for units.utils.parse_query. """
 
-    def test_queries(self):
-        """ Test all possible queries. """
+    def test_valids(self):
+        """ Test all cases that don"t include success. """
         # Given
         data = {
             "multiple": {
                 "query": "gold=5, blue!=3,    red>=1,energy:2",
-                "expected_url": [
-                    ['gold', '=', '5'],
-                    ['blue', '!=', '3'],
-                    ['red', '>=', '1'],
-                    ['energy', ':', '2'],
+                "expected_result": [
+                    ["gold", "=", "5"],
+                    ["blue", "!=", "3"],
+                    ["red", ">=", "1"],
+                    ["energy", ":", "2"],
                     ]
                 },
             "single": {
                 "query": "gold=5",
-                "expected_url": [['gold', '=', '5']]
+                "expected_result": [["gold", "=", "5"]]
                 },
             "no_operator": {
                 "query": "gold",
-                "expected_url": [['gold']]
+                "expected_result": [["gold"]]
                 },
             }
 
@@ -78,4 +79,90 @@ class ParseQueryCleanTests(unittest.TestCase):
             with self.subTest(name):
                 self.assertEqual(
                     list(parse_query(params["query"])),
-                    params["expected_url"])
+                    params["expected_result"])
+
+
+class FilterToDjangoDirtyTests(unittest.TestCase):
+    """ Tests failure cases for units.utils.filter_to_lookup. """
+
+    def test_invalids(self):
+        """ Test all cases that include some failure. """
+        # Given
+        data = {
+            "empty": {
+                "filter": [],
+                "expected_result": {},
+                },
+            "no_value": {
+                "filter": ["gold", "="],
+                "expected_result": {},
+                },
+            "too_many": {
+                "filter": ["gold", "=", "5", "6"],
+                "expected_result": {},
+                },
+            "bad_operator": {
+                "filter": ["gold", "invalid", "5"],
+                "expected_result": {},
+                },
+            }
+
+        # When/Then
+        for name, params in data.items():
+            with self.subTest(name):
+                self.assertEqual(
+                    filter_to_lookup(params["filter"]),
+                    params["expected_result"])
+
+
+class FilterToDjangoCleanTests(unittest.TestCase):
+    """ Tests success cases for units.utils.filter_to_lookup. """
+
+    def test_valids(self):
+        """ Test all cases that don't include failure. """
+        # Given
+        data = {
+            "equal": {
+                "filter": ["name", "=", "drone"],
+                "expected_result": {"name__icontains": "drone"},
+                },
+            "colon": {
+                "filter": ["name", ":", "drone"],
+                "expected_result": {"name__icontains": "drone"},
+                },
+            "greater_than": {
+                "filter": ["name", ">", "drone"],
+                "expected_result": {"name__gt": "drone"},
+                },
+            "greater_than_or_equal": {
+                "filter": ["name", ">=", "drone"],
+                "expected_result": {"name__gte": "drone"},
+                },
+            "less_than": {
+                "filter": ["name", "<", "drone"],
+                "expected_result": {"name__lt": "drone"},
+                },
+            "less_than_or_equal": {
+                "filter": ["name", "<=", "drone"],
+                "expected_result": {"name__lte": "drone"},
+                },
+            "different_than_1": {
+                "filter": ["name", "!=", "drone"],
+                "expected_result": {"name__icontains": "drone"},
+                },
+            "different_than_2": {
+                "filter": ["name", "<>", "drone"],
+                "expected_result": {"name__icontains": "drone"},
+                },
+            "number": {
+                "filter": ["name", ":", "5"],
+                "expected_result": {"name__icontains": 5},
+                },
+            }
+
+        # When/Then
+        for name, params in data.items():
+            with self.subTest(name):
+                self.assertEqual(
+                    filter_to_lookup(params["filter"]),
+                    params["expected_result"])
--- units/utils.py
+++ units/utils.py
@@ -1,7 +1,9 @@
 """ Utilities for papi.units """
 from typing import (
+    Dict,
     Iterator,
     List,
+    Union,
     )
 from pyparsing import (
     alphanums,
@@ -15,7 +17,17 @@ from pyparsing import (
     )
 
 
-SEARCH_OPERATORS = oneOf(": < = > >= <= != <>")
+OPERATORS_MAP = {
+    ":": "__icontains",
+    "=": "__icontains",
+    ">": "__gt",
+    ">=": "__gte",
+    "<": "__lt",
+    "<=": "__lte",
+    "!=": "__icontains",
+    "<>": "__icontains",
+    }
+SEARCH_OPERATORS = oneOf(" ".join(OPERATORS_MAP.keys()))
 SEARCH_FILTER = Word(alphas) + Optional(SEARCH_OPERATORS + Word(alphanums))
 SEARCH_QUERY = delimitedList(Combine(SEARCH_FILTER), delim=",")
 
@@ -36,14 +48,15 @@ def parse_query(raw_query: str) -> Iterator[List[str]]:
     Examples
     --------
     input:
-        "gold=5, blue!=3,    red>=1,energy:2"
+        "gold=5, blue!=3,    red>=1,drone,energy:2"
 
     output:
         [
-            ['gold', '=', '5'],
-            ['blue', '!=', '3'],
-            ['red', '>=', '1'],
-            ['energy', ':', '2'],
+            ["gold", "=", "5"],
+            ["blue", "!=", "3"],
+            ["red", ">=", "1"],
+            ["drone"],
+            ["energy", ":", "2"],
         ]
 
     """
@@ -55,3 +68,37 @@ def parse_query(raw_query: str) -> Iterator[List[str]]:
             )
     except ParseException:
         return (_ for _ in [])  # empty iterator
+
+
+def filter_to_lookup(data: List[str]) -> Dict[str, Union[str, int]]:
+    """
+    Get dict with expected format for a Django ORM query/filter lookup.
+
+    Expected data format: ["field name", "operator", "value"]
+
+    Parameters
+    ----------
+    data : list(str)
+        Filter in raw format.
+
+    Returns
+    -------
+    dict()
+
+    Examples
+    --------
+    input:
+        ["gold", "=", "5"]
+
+    output:
+        {"gold__iexact": "5:}
+
+    """
+    if len(data) != 3 or data[1] not in OPERATORS_MAP:
+        return {}
+
+    field, operator, value = data
+    return {
+        f"{field}{OPERATORS_MAP[operator]}":
+        int(value) if value.isdigit() else value
+        }
