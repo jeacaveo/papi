181563b7220f7823698eaef0dff993073db86ea9
Jean Ventura <jv@venturasystems.net>
2019-11-22 22:03:07 -0800

    Add includes/excludes filter to /latest/units/ endpoint

---
+++ units/tests/test_unit_views.py
@@ -0,0 +1,105 @@
+""" Tests for units.views """
+import unittest
+from mock import (
+    call,
+    MagicMock,
+    )
+
+from django.urls import reverse
+from rest_framework.test import APIRequestFactory
+
+from units.views import LatestUnitVersionViewSet
+
+
+class LatestUnitVersionCleanTests(unittest.TestCase):
+    """ Tests success cases for units.views.LatestUnitVersionViewSet. """
+
+    def setUp(self):
+        self.factory = APIRequestFactory()
+        self.url = reverse("latestunitversionview-list")
+        self.view = LatestUnitVersionViewSet()
+        self.queryset_mock = MagicMock()
+
+    def test_no_query(self):
+        """ Test queryset when no query params provided. """
+        # Given
+        request = self.factory.get(self.url, {})
+        self.view.request = request
+        self.view.queryset = self.queryset_mock
+        expected_result = MagicMock()
+
+        self.queryset_mock.filter.return_value = self.queryset_mock
+        self.queryset_mock.exclude.return_value = expected_result
+
+        # When
+        result = LatestUnitVersionViewSet.get_queryset(self.view)
+
+        # Then
+        self.assertEqual(result, expected_result)
+        self.queryset_mock.assert_has_calls([
+            call.filter(),
+            call.exclude(),
+            ])
+
+    def test_includes(self):
+        """ Test queryset when only includes are provided. """
+        # Given
+        request = self.factory.get(self.url, {"q": "gold=5"})
+        self.view.request = request
+        self.view.queryset = self.queryset_mock
+        expected_result = MagicMock()
+
+        self.queryset_mock.filter.return_value = self.queryset_mock
+        self.queryset_mock.exclude.return_value = expected_result
+
+        # When
+        result = LatestUnitVersionViewSet.get_queryset(self.view)
+
+        # Then
+        self.assertEqual(result, expected_result)
+        self.queryset_mock.assert_has_calls([
+            call.filter(gold__icontains=5),
+            call.exclude(),
+            ])
+
+    def test_excludes(self):
+        """ Test queryset when only excludes are provided. """
+        # Given
+        request = self.factory.get(self.url, {"q": "gold!=5"})
+        self.view.request = request
+        self.view.queryset = self.queryset_mock
+        expected_result = MagicMock()
+
+        self.queryset_mock.filter.return_value = self.queryset_mock
+        self.queryset_mock.exclude.return_value = expected_result
+
+        # When
+        result = LatestUnitVersionViewSet.get_queryset(self.view)
+
+        # Then
+        self.assertEqual(result, expected_result)
+        self.queryset_mock.assert_has_calls([
+            call.filter(),
+            call.exclude(gold__icontains=5),
+            ])
+
+    def test_both(self):
+        """ Test queryset when both includes and excludes are provided. """
+        # Given
+        request = self.factory.get(self.url, {"q": "gold=5,gold!=5"})
+        self.view.request = request
+        self.view.queryset = self.queryset_mock
+        expected_result = MagicMock()
+
+        self.queryset_mock.filter.return_value = self.queryset_mock
+        self.queryset_mock.exclude.return_value = expected_result
+
+        # When
+        result = LatestUnitVersionViewSet.get_queryset(self.view)
+
+        # Then
+        self.assertEqual(result, expected_result)
+        self.queryset_mock.assert_has_calls([
+            call.filter(gold__icontains=5),
+            call.exclude(gold__icontains=5),
+            ])
--- units/views.py
+++ units/views.py
@@ -1,14 +1,26 @@
 """ Views for papi.units """
 # pylint: disable=too-many-ancestors
+from django.db.models import QuerySet
 from rest_framework import viewsets
 
 from units.models import LatestUnitVersionView
 from units.serializers import LatestUnitVersionViewSerializer
+from units.utils import includes_excludes
 
 
 class LatestUnitVersionViewSet(viewsets.ReadOnlyModelViewSet):  # type: ignore
     """ API endpoint for latest version of a unit. """
 
     # pylint: disable=no-member
-    queryset = LatestUnitVersionView.objects.all().order_by("name")
+    queryset = LatestUnitVersionView.objects.filter()
     serializer_class = LatestUnitVersionViewSerializer
+
+    def get_queryset(self) -> QuerySet:
+        """
+        Custom filtering.
+
+        Split filters into includes and excludes depeding on the operator
+
+        """
+        includes, excludes = includes_excludes(self.request.GET.get("q") or "")
+        return self.queryset.filter(**includes).exclude(**excludes)
